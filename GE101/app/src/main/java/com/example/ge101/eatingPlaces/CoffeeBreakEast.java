90a7da832c75e1b8a4f49936408fced2af37b34c
Berke Uçar <berke.ucar@ug.bilkent.edu.tr>
2020-05-16 00:25:52 +0300

    Coffee Break (both campuses) and menu added

--- GE101/app/src/main/java/com/example/ge101/eatingPlaces/CoffeeBreakEast.java
+++ GE101/app/src/main/java/com/example/ge101/eatingPlaces/CoffeeBreakEast.java
@@ -2,10 +2,8 @@ package com.example.ge101.eatingPlaces;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,8 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ge101.R;
 
-public class MozartCafe extends AppCompatActivity {
-
+public class CoffeeBreakEast extends AppCompatActivity {
     // properties
     private ImageView menu;
     private long mLastClickTime = 0;
@@ -25,29 +22,10 @@ public class MozartCafe extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mozart_cafe_screen);
+        setContentView(R.layout.coffee_break_menu);
 
-        // Button for Mozart Cafe
-        menu =(ImageView) findViewById(R.id.mozart_menu_button);
-        menu.setOnClickListener(new View.OnClickListener()
-        {
-            /**
-             * the method that makes button to open the school cafeteria's menu when clicked
-             * @param v
-             */
-            @Override
-            public void onClick (View v)
-            {
-                // Preventing multiple clicks, using threshold of 1 second
-                if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
-                    return;
-                }
-                mLastClickTime = SystemClock.elapsedRealtime();
-                openMenu();
-            }
-        });
 
-        menu =(ImageView) findViewById(R.id.mozart_menu_button);
+        menu =(ImageView) findViewById(R.id.cbmenubutton);
         menu.setOnClickListener(new View.OnClickListener()
         {
             /**
@@ -68,9 +46,8 @@ public class MozartCafe extends AppCompatActivity {
     }
 
     private void openMenu() {
-        // creates intent (action) and opens it (for this Mozart's menu)
-        Intent intent = new Intent(this, MozartCafeMenu.class);
+        // creates intent (action) and opens it (for this condition school cafeteria's menu)
+        Intent intent = new Intent(this, CoffeeBreakMenu.class);
         startActivity(intent);
     }
-
 }
